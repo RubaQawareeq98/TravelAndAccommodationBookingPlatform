2c9d3d8ab207e31220a170b6146889027a105033
ruba <s12028675@stu.najah.edu>
2025-06-07 02:16:07 +0300

    Refactor Relation between Amenity & RoomInfo entities

--- TravelAndAccommodationBookingPlatform.Domain/Entities/Amenity.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Entities/Amenity.cs
@@ -4,6 +4,5 @@ public class Amenity : BaseEntity
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Guid RoomId { get; set; }
-    public Room Room { get; set; }
+    public virtual ICollection<RoomInfo> RoomInfos { get; set; } =[];
 }
