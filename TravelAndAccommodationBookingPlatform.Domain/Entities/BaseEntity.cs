90ce219de924d528c9c97648d8a5465ae4e6c0d1
ruba <s12028675@stu.najah.edu>
2025-06-06 15:45:05 +0300

    Add Domain Project

---
+++ TravelAndAccommodationBookingPlatform.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace TravelAndAccommodationBookingPlatform.Domain.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ TravelAndAccommodationBookingPlatform.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+using TravelAndAccommodationBookingPlatform.Domain.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? Address { get; set; }
+    public required string PhoneNumber { get; set; }
+    public UserRole Role { get; set; } = UserRole.User;
+}
