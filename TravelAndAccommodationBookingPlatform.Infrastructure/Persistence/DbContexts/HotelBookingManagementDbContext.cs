337f3c742670cf3320c7e397f985f07adec73b21
ruba <s12028675@stu.najah.edu>
2025-06-07 12:04:10 +0300

    Create Infrastructure Project & HotelBookingManagementDbContext

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/DbContexts/HotelBookingManagementDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.DbContexts;
+
+public class HotelBookingManagementDbContext (DbContextOptions<HotelBookingManagementDbContext> options) : DbContext (options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomInfo> RoomInfos { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Amenity> Amenities { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<PaymentDetail> PaymentDetails { get; set; }
+}
