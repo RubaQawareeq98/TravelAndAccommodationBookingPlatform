479d39af038b29e40cf63c538d1c0ea536a7b076
ruba <s12028675@stu.najah.edu>
2025-06-08 12:16:11 +0300

    Add DbContext Factory

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/DbContexts/HotelBookingManagementDbContextFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.DbContexts;
+
+public class HotelBookingManagementDbContextFactory : IDesignTimeDbContextFactory<HotelBookingManagementDbContext>
+{
+    public HotelBookingManagementDbContext CreateDbContext(string[] args)
+    {
+    //    var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../CarRentalSystem");
+
+        var configuration = new ConfigurationBuilder()
+          //  .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var optionsBuilder = new DbContextOptionsBuilder<HotelBookingManagementDbContext>();
+        var connectionString = configuration.GetConnectionString("sqlConnectionString");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new HotelBookingManagementDbContext(optionsBuilder.Options);
+    }
+}
