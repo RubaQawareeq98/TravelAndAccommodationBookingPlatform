92fd9c72c52c0f64e3b3f84cb052b9ebf4397e60
ruba <s12028675@stu.najah.edu>
2025-06-11 10:01:48 +0300

    Add User Service

---
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,11 @@
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.Services;
+
+public interface IUserService
+{
+    Task<User> GetUserByIdAsync(Guid userId);
+    Task<User> GetUserByCredentialsAsync(string email, string password);
+    Task AddUserAsync(User user);
+    Task UpdateUserAsync(User user);
+}
---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Services/Users/UserService.cs
@@ -0,0 +1,50 @@
+using TravelAndAccommodationBookingPlatform.Application.Common.Interfaces;
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Services;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Users;
+
+public class UserService(IUserRepository userRepository, IPasswordHashingService passwordHashingService) : IUserService
+{
+    public async Task<User> GetUserByIdAsync(Guid userId)
+    {
+        var user = await userRepository.GetUserById(userId);
+        if (user is null)
+        {
+            throw new InvalidCastException();
+        }
+        return user;
+    }
+
+    public async Task<User> GetUserByCredentialsAsync(string email, string password)
+    {
+        var user = await userRepository.GetUserByEmail(email);
+        ArgumentNullException.ThrowIfNull(user);
+        
+        var isMatchedPassword = passwordHashingService.VerifyPassword(password, user.Password);
+        
+        if (isMatchedPassword)
+        {
+            return user;
+        }
+
+        throw new InvalidDataException();
+    }
+
+    public async Task AddUserAsync(User user)
+    {
+        var existUser = await userRepository.GetUserByEmail(user.Email);
+        if (existUser is not null)
+        {
+            throw new InvalidOperationException($"User with email {user.Email} already exists.");
+        }
+        user.Password = passwordHashingService.HashPassword(user.Password);
+        await userRepository.CreateUser(user);
+    }
+
+    public async Task UpdateUserAsync(User user)
+    {
+        await userRepository.UpdateUser(user);
+    }
+}
