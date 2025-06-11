ba8103e4b11f8ebbd5826045d93fd12c44192fbd
ruba <s12028675@stu.najah.edu>
2025-06-11 18:16:34 +0300

    Add Jwt auth options to appsettings file

--- TravelAndAccommodationBookingPlatform.Infrastructure/Configurations/InfrastructureConfigurations.cs
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Configurations/InfrastructureConfigurations.cs
@@ -1,15 +1,18 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TravelAndAccommodationBookingPlatform.Infrastructure.JwtAuth.Configurations;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.PersistenceConfigurations;
-using TravelAndAccommodationBookingPlatform.Infrastructure.Services.Security;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Security.Configurations;
 
 namespace TravelAndAccommodationBookingPlatform.Infrastructure.Configurations;
 
 public static class InfrastructureConfigurations
 {
-    public static IServiceCollection AddInfrastructureConfigurations(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureConfigurations(this WebApplicationBuilder builder, IConfiguration configuration)
     {
-        return services.AddPersistenceRepositories()
+        return builder.AddJwtParams()
+            .AddPersistenceRepositories()
             .AddPersistenceServices()
             .AddHashingConfiguration()
             .AddPersistenceDbContexts(configuration);
