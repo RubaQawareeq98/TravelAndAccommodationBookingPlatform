9c78c439f483880ff75d795ce774400b5d4b425c
ruba <s12028675@stu.najah.edu>
2025-06-11 12:28:18 +0300

    Add Register Request Validator

---
+++ TravelAndAccommodationBookingPlatform.Api/Authentication/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,40 @@
+using FluentValidation;
+using TravelAndAccommodationBookingPlatform.Api.Authentication.Dtos;
+
+namespace TravelAndAccommodationBookingPlatform.Api.Authentication.Validators;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .WithMessage("First name cannot be empty")
+            .MaximumLength(50)
+            .WithMessage("First name cannot be more than 50 characters");
+        
+        RuleFor(r => r.LastName)
+            .NotEmpty()
+            .WithMessage("Last name cannot be empty")
+            .MaximumLength(50)
+            .WithMessage("Last name cannot be more than 50 characters");
+        
+        RuleFor(r => r.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Invalid email address")
+            .WithErrorCode("EMAIL_REQUIRED")
+            .WithSeverity(Severity.Error);
+        
+        RuleFor(r => r.Password)
+            .NotEmpty()
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_+=\[\]{};:'"",.<>?/\\|`~]).{8,}$")
+            .WithMessage("Password must be at least 8 characters long and contain an uppercase letter, a lowercase letter, a number, and a special character.")
+            .WithErrorCode("WEAK_PASSWORD")
+            .WithSeverity(Severity.Error);
+
+        RuleFor(r => r.PhoneNumber)
+            .NotEmpty()
+            .WithMessage("Phone number cannot be empty");
+    }
+}
