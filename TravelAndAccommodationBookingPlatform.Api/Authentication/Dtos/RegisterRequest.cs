8df43b9b56f8f0674b5c6a960c1aec99ff083149
ruba <s12028675@stu.najah.edu>
2025-06-11 12:27:49 +0300

    Add User Register Request Dto

---
+++ TravelAndAccommodationBookingPlatform.Api/Authentication/Dtos/RegisterRequest.cs
@@ -0,0 +1,14 @@
+using TravelAndAccommodationBookingPlatform.Domain.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Api.Authentication.Dtos;
+
+public class RegisterRequest
+{
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public UserRole? Role { get; set; } = UserRole.User;
+}
