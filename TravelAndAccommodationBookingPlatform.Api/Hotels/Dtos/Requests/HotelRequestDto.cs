cf2825eb972c3b4b44eba821cb740f8f0540c703
ruba <s12028675@stu.najah.edu>
2025-06-13 00:56:59 +0300

    Add hotels controller & add new hotel endpoint

---
+++ TravelAndAccommodationBookingPlatform.Api/Hotels/Dtos/Requests/HotelRequestDto.cs
@@ -0,0 +1,18 @@
+using TravelAndAccommodationBookingPlatform.Domain.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Api.Hotels.Dtos.Requests;
+
+public class HotelRequestDto
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string? Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public int StarRating { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    public int TotalRooms { get; set; }
+    public HotelType HotelType { get; set; }
+    public Guid CityId { get; set; }
+    public Guid OwnerId { get; set; }
+}
