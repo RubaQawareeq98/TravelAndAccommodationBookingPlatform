56028a00d8a31edc73c65f4a6f90434c67c15309
ruba <s12028675@stu.najah.edu>
2025-06-13 12:36:09 +0300

    Implement city service methods

--- TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Services/Cities/CityService.cs
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Services/Cities/CityService.cs
@@ -1,6 +1,49 @@
+using Sieve.Models;
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+using TravelAndAccommodationBookingPlatform.Domain.Exceptions;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Services;
+
 namespace TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Cities;
 
-public class CityService
+public class CityService(ICityRepository cityRepository) : ICityService
 {
-    
+    public async Task AddCityAsync(City city)
+    {
+        await cityRepository.AddCity(city);
+    }
+
+    public async Task UpdateCityAsync(Guid cityId, City city)
+    {
+        var isCityExist = await cityRepository.IsCityExist(cityId);
+        if (isCityExist is false)
+        {
+            throw new NotFoundException($"City with id: {cityId} does not exist.");
+        }
+        
+        await cityRepository.UpdateCity(city);
+    }
+
+    public async Task DeleteCityAsync(Guid cityId)
+    {
+        var city = await GetCityByIdAsync(cityId);
+        
+        await cityRepository.DeleteCity(city);
+    }
+
+    public async Task<List<City>> GetCitiesAsync(SieveModel sieveModel)
+    {
+        return await cityRepository.GetCities(sieveModel);
+    }
+
+    public async Task<City> GetCityByIdAsync(Guid cityId)
+    {
+        var city = await cityRepository.GetCityById(cityId);
+        if (city is null)
+        {
+            throw new NotFoundException($"City with id: {cityId} does not exist.");
+        }
+        
+        return city;
+    }
 }
