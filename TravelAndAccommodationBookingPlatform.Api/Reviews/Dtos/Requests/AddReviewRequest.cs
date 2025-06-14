5b53e364b1e38f94c94974be8e40734127d204fc
ruba <s12028675@stu.najah.edu>
2025-06-14 22:47:08 +0300

    Add reviews controller & dto with validator

---
+++ TravelAndAccommodationBookingPlatform.Api/Reviews/Dtos/Requests/AddReviewRequest.cs
@@ -0,0 +1,10 @@
+
+namespace TravelAndAccommodationBookingPlatform.Api.Reviews.Dtos.Requests;
+
+public class AddReviewRequest
+{
+    public Guid UserId { get; set; }
+    public Guid HotelId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public int Rating { get; set; }
+}
