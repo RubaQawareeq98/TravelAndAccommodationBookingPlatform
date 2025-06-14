879d352d852d0e6945f248ae84dc2ddb92161833
ruba <s12028675@stu.najah.edu>
2025-06-14 12:02:23 +0300

    Refactor owner repository: add new method

--- TravelAndAccommodationBookingPlatform.Domain/Interfaces/Persistence/Repositories/IOwnerRepository.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/Persistence/Repositories/IOwnerRepository.cs
@@ -1,3 +1,4 @@
+using Sieve.Models;
 using TravelAndAccommodationBookingPlatform.Domain.Entities;
 
 namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.Persistence.Repositories;
@@ -8,4 +9,5 @@ public interface IOwnerRepository
     Task UpdateOwner(Owner owner);
     Task DeleteOwner(Owner owner);
     Task<Owner?> GetOwner(Guid id);
+    Task<List<Owner>> GetOwners(SieveModel sieveModel);
 }
--- TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Repositories/Owners/OwnerRepository.cs
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Repositories/Owners/OwnerRepository.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Sieve.Models;
+using Sieve.Services;
 using TravelAndAccommodationBookingPlatform.Domain.Entities;
 using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Persistence.Repositories;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.DbContexts;
 
 namespace TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Owners;
 
-public class OwnerRepository(HotelBookingManagementDbContext dbContext) : IOwnerRepository
+public class OwnerRepository(HotelBookingManagementDbContext dbContext, ISieveProcessor sieveProcessor) : IOwnerRepository
 {
     public async Task AddOwner(Owner owner)
     {
@@ -28,5 +30,12 @@ public class OwnerRepository(HotelBookingManagementDbContext dbContext) : IOwner
     public async Task<Owner?> GetOwner(Guid id)
     {
         return await dbContext.Owners.FirstOrDefaultAsync(o => o.Id == id && !o.IsDeleted);
+    }
+
+    public async Task<List<Owner>> GetOwners(SieveModel sieveModel)
+    {
+        var query = dbContext.Owners.AsQueryable();
+        query = sieveProcessor.Apply(sieveModel, query);
+        return await query.ToListAsync();
     }
 }
