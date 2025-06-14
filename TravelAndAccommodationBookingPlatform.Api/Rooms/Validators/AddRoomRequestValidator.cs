6e256ba3b8b876937bb322a32f679060fa52e791
ruba <s12028675@stu.najah.edu>
2025-06-14 15:17:34 +0300

    Add room creation request dto & calidator

---
+++ TravelAndAccommodationBookingPlatform.Api/Rooms/Validators/AddRoomRequestValidator.cs
@@ -0,0 +1,47 @@
+using FluentValidation;
+using TravelAndAccommodationBookingPlatform.Api.RoomInfos.Dtos.Requests;
+
+namespace TravelAndAccommodationBookingPlatform.Api.Rooms.Validators;
+
+public class AddRoomInfoRequestValidator : AbstractValidator<AddRoomInfoRequest>
+{
+    public AddRoomInfoRequestValidator()
+    {
+        RuleFor(ri => ri.ChildrenCapacity)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Room Capacity must be greater than 0")
+            .WithErrorCode("InvalidCapacity");
+        
+        RuleFor(ri => ri.AdultsCapacity)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Room Capacity must be greater than 0")
+            .WithErrorCode("InvalidCapacity");
+        
+        RuleFor(ri => ri.RoomType)
+            .NotEmpty()
+            .IsInEnum()
+            .WithMessage("Room Type is required")
+            .WithErrorCode("InvalidRoomType");
+
+        RuleFor(ri => ri.Name)
+            .NotEmpty()
+            .WithMessage("Name is required");
+        
+        RuleFor(ri => ri.Description)
+            .MaximumLength(400)
+            .WithMessage("Description must be less than 400 characters")
+            .WithErrorCode("InvalidDescription")
+            .WithSeverity(Severity.Warning);
+        
+        RuleFor(ri => ri.HotelId)
+            .NotEmpty()
+            .WithMessage("Hotel Id is required")
+            .WithErrorCode("InvalidHotelId")
+            .WithSeverity(Severity.Error);
+
+        RuleFor(ri => ri.PricePerNight)
+            .NotEmpty()
+            .GreaterThan(0)
+            .WithMessage("Price Per Night must be greater than 0");
+    }
+}
