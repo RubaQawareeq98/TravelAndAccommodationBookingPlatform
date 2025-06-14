b47c21dc94d986ff3e932e639935f6956893ff08
ruba <s12028675@stu.najah.edu>
2025-06-15 00:38:12 +0300

    Register repos & services

--- TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/PersistenceConfigurations/RepositoriesConfiguration.cs
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/PersistenceConfigurations/RepositoriesConfiguration.cs
@@ -1,8 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Persistence.Repositories;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Amenities;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Bookings;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Cities;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Discounts;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Hotels;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Owners;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Reviews;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.RoomInfos;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Rooms;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Repositories.Users;
@@ -19,6 +23,10 @@ public static class RepositoriesConfiguration
         services.AddScoped<IOwnerRepository, OwnerRepository>();
         services.AddScoped<IRoomInfoRepository, RoomInfoRepository>();
         services.AddScoped<IRoomRepository, RoomRepository>();
+        services.AddScoped<IReviewRepository, ReviewRepository>();
+        services.AddScoped<IDiscountRepository, DiscountRepository>();
+        services.AddScoped<IAmenityRepository, AmenityRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
         
         return services;
     }
--- TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/PersistenceConfigurations/ServicesConfiguration.cs
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/PersistenceConfigurations/ServicesConfiguration.cs
@@ -1,8 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Persistence.Services;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Amenities;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Bookings;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Cities;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Discounts;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Hotels;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Owners;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Reviews;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.RoomInfos;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Rooms;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Users;
@@ -19,6 +23,10 @@ public static class ServicesConfiguration
         services.AddScoped<IOwnerService, OwnerService>();
         services.AddScoped<IRoomInfoService, RoomInfoService>();
         services.AddScoped<IRoomService, RoomService>();
+        services.AddScoped<IReviewService, ReviewService>();
+        services.AddScoped<IDiscountService, DiscountService>();
+        services.AddScoped<IAmenityService, AmenityService>();
+        services.AddScoped<IBookingService, BookingService>();
         
         return services;
     }
