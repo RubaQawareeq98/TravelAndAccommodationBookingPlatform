4db165e083c28d563495b3f1b2c6b43df27b6b9a
ruba <s12028675@stu.najah.edu>
2025-06-14 12:02:02 +0300

    Add & implement owner service

---
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/Persistence/Services/IOwnerService.cs
@@ -0,0 +1,6 @@
+namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.Persistence.Services;
+
+public interface IOwnerService
+{
+    
+}
---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Services/Owners/OwnerService.cs
@@ -0,0 +1,6 @@
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Persistence.Services.Owners;
+
+public class OwnerService
+{
+    
+}
