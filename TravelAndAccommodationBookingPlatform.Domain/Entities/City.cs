337a8c622937b5f1e8492ce808d132535dc6f464
ruba <s12028675@stu.najah.edu>
2025-06-16 16:42:28 +0300

    Remove required property in Hotel entity

--- TravelAndAccommodationBookingPlatform.Domain/Entities/City.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Entities/City.cs
@@ -2,8 +2,8 @@ namespace TravelAndAccommodationBookingPlatform.Domain.Entities;
 
 public class City : AuditableSoftDeleteBaseEntity
 {
-    public required string? Name { get; set; }
-    public required string? Country { get; set; }
+    public required string Name { get; set; }
+    public required string Country { get; set; }
     public required string PostalCode { get; set; }
     public virtual ICollection<Hotel> Hotels { get; set; } = [];
 }
