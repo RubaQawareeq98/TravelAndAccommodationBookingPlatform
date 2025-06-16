24a9a2211bec85b8a0d4a127430aba9284fc6d1a
ruba <s12028675@stu.najah.edu>
2025-06-16 16:31:38 +0300

    Add recently visited hotels dto in application layer

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Migrations/20250616103055_AddIndexes.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailUrl",
+                table: "Cities");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_EndDate",
+                table: "Discounts",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_StartDate",
+                table: "Discounts",
+                column: "StartDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_EndDate",
+                table: "Discounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_StartDate",
+                table: "Discounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailUrl",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
