5a267ea14765b64828391f7a6bd8b7a775b1ecdb
ruba <s12028675@stu.najah.edu>
2025-06-19 08:53:03 +0300

    Update Room entity: Add RowVersion property

--- TravelAndAccommodationBookingPlatform.Domain/Entities/Room.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Entities/Room.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TravelAndAccommodationBookingPlatform.Domain.Entities;
 
 public class Room : AuditableSoftDeleteBaseEntity
@@ -6,4 +8,7 @@ public class Room : AuditableSoftDeleteBaseEntity
     public RoomInfo RoomInfo { get; set; }
     public Guid RoomInfoId { get; set; }
     public ICollection<Booking> Bookings { get; set; } = [];
+    
+    [Timestamp]
+    public byte[] RowVersion { get; set; }
 }
