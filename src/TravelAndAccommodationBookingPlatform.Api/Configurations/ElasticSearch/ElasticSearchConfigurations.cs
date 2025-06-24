ed41a79d820437b9b51549d09bec3e94807bba05
ruba <s12028675@stu.najah.edu>
2025-06-24 20:33:50 +0300

    Add Serilog & ElasticSearch settings

---
+++ src/TravelAndAccommodationBookingPlatform.Api/Configurations/ElasticSearch/ElasticSearchConfigurations.cs
@@ -0,0 +1,9 @@
+namespace TravelAndAccommodationBookingPlatform.Api.Configurations.ElasticSearch;
+
+public class ElasticSearchConfigurations
+{
+    public string ElasticSearchUri { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string IndexFormat { get; set; } = string.Empty;
+}
