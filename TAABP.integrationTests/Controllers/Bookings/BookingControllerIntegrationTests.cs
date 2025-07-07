161b6f8d378071b2ac0143836b9def006eb9546f
ruba <s12028675@stu.najah.edu>
2025-07-07 21:05:54 +0300

    Add integration tests for Add booking endpoint

--- TAABP.integrationTests/Controllers/Bookings/BookingControllerIntegrationTests.cs
+++ TAABP.integrationTests/Controllers/Bookings/BookingControllerIntegrationTests.cs
@@ -6,14 +6,21 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
 using TAABP.integrationTests.Controllers.Bookings.Utils;
+using TAABP.integrationTests.Controllers.Hotels.Utils;
+using TAABP.integrationTests.Controllers.RoomCategories.Utils;
+using TAABP.integrationTests.Controllers.Rooms.Utils;
 using TAABP.integrationTests.Controllers.Users.Utils;
 using TAABP.integrationTests.Fixtures;
 using TAABP.integrationTests.Handlers;
 using TAABP.integrationTests.Helpers;
 using TAABP.integrationTests.Shared;
 using TravelAndAccommodationBookingPlatform.Api;
+using TravelAndAccommodationBookingPlatform.Api.Bookings.Dtos.Requests;
 using TravelAndAccommodationBookingPlatform.Api.Bookings.Dtos.Responses;
+using TravelAndAccommodationBookingPlatform.Application.Emails.Interfaces;
 using TravelAndAccommodationBookingPlatform.Domain.Entities;
 using TravelAndAccommodationBookingPlatform.Domain.Enums;
 
@@ -26,9 +33,8 @@ public class BookingControllerIntegrationTests : IClassFixture<SqlServerFixture>
     private readonly string _baseUrl;
     private readonly Guid _userId;
     private readonly WebApplicationFactory<Program> _factory;
-    private readonly SqlServerFixture _sqlServerFixture;
-    
-    
+
+
     public BookingControllerIntegrationTests(SqlServerFixture sqlServerFixture)
     {
         _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
@@ -36,8 +42,12 @@ public class BookingControllerIntegrationTests : IClassFixture<SqlServerFixture>
         
         _userId = _fixture.Create<Guid>();
         _baseUrl = $"/api/users/{_userId}/bookings";
-        _sqlServerFixture = sqlServerFixture;
         _factory = sqlServerFixture.Factory;
+        
+        var mockEmailService = new Mock<IEmailService>();
+        mockEmailService
+            .Setup(e => e.SendConfirmationEmail(It.IsAny<User>(), It.IsAny<string>(), It.IsAny<Booking>(), It.IsAny<byte[]>()))
+            .Returns(Task.CompletedTask);
 
         _client = _factory.WithWebHostBuilder(builder =>
         {
@@ -46,10 +56,13 @@ public class BookingControllerIntegrationTests : IClassFixture<SqlServerFixture>
                 services.AddAuthentication("TestAuthScheme")
                     .AddScheme<AuthenticationSchemeOptions, AuthenticationHandlerTest>(
                         "TestAuthScheme", _ => { });
+                
+                services.RemoveAll(typeof(IEmailService));
+                services.AddScoped(_ => mockEmailService.Object);
             });
         }).CreateClient();
     }
-
+    
     [Fact]
     public async Task GetBookings_ShouldReturnsOkWithBookings_IfValidRequest()
     {
@@ -78,6 +91,71 @@ public class BookingControllerIntegrationTests : IClassFixture<SqlServerFixture>
         
         // Assert
         responseBookings.Should().NotBeNull().And.HaveCount(bookings.Count);
+    }
+
+    [Fact]
+    public async Task AddBooking_ShouldReturnCreatedAndPersistBooking_WhenValidRequest()
+    {
+        // Arrange
+        var user = _fixture.Build<User>()
+            .With(u => u.Id, _userId)
+            .Without(u => u.Bookings)
+            .Create();
+        await UserTestUtilities.AddTestUsers([user], _factory);
+    
+        var hotel = _fixture.Build<Hotel>()
+            .Without(h => h.RoomCategories)
+            .Without(h => h.Reviews)
+            .Without(h => h.Gallery)
+            .Without(h => h.Bookings)
+            .Create();
+        await HotelTestUtilities.AddTestHotels([hotel], _factory);
+        
+        var roomCategory = _fixture.Build<RoomCategory>()
+            .With(r => r.HotelId, hotel.Id)
+            .Without(r => r.Hotel)
+            .Without(r => r.Discounts)
+            .Without(r => r.Amenities)
+            .Without(r => r.Rooms)
+            .Create();
+    
+        await RoomCategoryTestUtilities.AddTestRoomCategories([roomCategory], _factory);
+    
+        var rooms = _fixture.Build<Room>()
+            .With(r => r.RoomCategoryId, roomCategory.Id)
+            .Without(r => r.RoomCategory)
+            .Without(r => r.Gallery)
+            .Without(r => r.Bookings)
+            .CreateMany(2)
+            .ToList();
+    
+        await RoomTestUtilities.AddTestRooms(rooms, _factory);
+    
+        var checkInDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(2));
+        var checkOutDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(5));
+    
+        var request = _fixture.Build<AddBookingRequest>()
+            .With(r => r.CheckInDate, checkInDate)
+            .With(r => r.CheckOutDate, checkOutDate)
+            .With(r => r.PaymentMethod, PaymentMethod.Cash.ToString())
+            .With(r => r.HotelId, hotel.Id)
+            .With(r => r.RoomsIds, rooms.Select(r => r.Id).ToList())
+            .Create();
+        
+        TestAuthenticationHeader.SetTestAuthHeader(_client, user.Id, UserRole.User);
+    
+        var url = $"/api/users/{user.Id}/bookings";
+    
+        // Act
+        var response = await _client.PostAsJsonAsync(url, request);
+    
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    
+        var bookingResponse = await response.Content.ReadFromJsonAsync<BookingResponse>();
+        bookingResponse.Should().NotBeNull();
+        bookingResponse.UserId.Should().Be(user.Id);
+        bookingResponse.HotelId.Should().Be(hotel.Id);
     }
     
     public Task InitializeAsync() => Task.CompletedTask;
